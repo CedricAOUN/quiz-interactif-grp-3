ae62388b02c13d9b4dafad67aaaab9581373c751
CedricA <cedric.j.aoun@gmail.com>
2025-01-10 11:17:58 +0100

    refactor: created questions.js for question list

---
+++ assets/js/questions.js
@@ -0,0 +1,27 @@
+let questions = [
+  {
+    text: "Quelle est la capitale de la France ?",
+    answers: ["Marseille", "Paris", "Lyon", "Bordeaux"],
+    correct: 1,
+    timeLimit: 10,
+  },
+  {
+    text: "Combien font 2 + 3 ?",
+    answers: ["3", "4", "5", "1"],
+    correct: 2,
+    timeLimit: 5,
+  },
+  {
+    text: "Combien font 2 + 3 ?",
+    answers: ["3", "4", "5", "1"],
+    correct: 2,
+    timeLimit: 5,
+  },
+  {
+    text: "Quelle est la capitale de la France ?",
+    answers: ["Marseille", "Paris", "Lyon", "Bordeaux"],
+    correct: 1,
+    timeLimit: 10,
+  },
+  // Ajoutez d’autres questions si besoin
+];
